dcd9b3c2d9be924332c0c972956a0c3fb45a4868
Éric <nascimento3ric@gmail.com>
2023-04-09 11:53:29 -0300

    página home aplicativo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,105 +1,57 @@
-import React, { useState } from 'react';
-import { IonButton, IonContent, IonItem, IonHeader, IonLabel, IonList, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import AddReminderModal from './addReminderModal';
+import { IonAvatar, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import ExploreContainer from '../components/ExploreContainer';
+import './Home.css';
 
-interface Reminder {
-  id: number;
-  title: string;
-  date: string;
-}
-
-const HomePage: React.FC = () => {
-  const [reminders, setReminders] = useState<Reminder[]>([]);
-  const [modalIsOpen, setModalIsOpen] = useState(false);
-
-  const handleSaveReminder = (title: string, date: string) => {
-    const newReminder: Reminder = {
-    id: Math.floor(Math.random() * 100000),
-    title,
-    date
-    };
-    const newReminders = [...reminders, newReminder];
-    setReminders(newReminders);
-    localStorage.setItem('reminders', JSON.stringify(newReminders));
-    setModalIsOpen(false);
-    };
-    
-    const handleDeleteReminder = (id: number) => {
-    const newReminders = reminders.filter(reminder => reminder.id !== id);
-    setReminders(newReminders);
-    localStorage.setItem('reminders', JSON.stringify(newReminders));
-    };
-    
-    const handleCheckReminders = () => {
-    const now = new Date().getTime();
-    reminders.forEach(reminder => {
-    const reminderDate = new Date(reminder.date).getTime();
-    if (reminderDate <= now) {
-    showNotification(reminder.title);
-    handleDeleteReminder(reminder.id);
-    }
-    });
-    };
-    
-    const showNotification = (title: string) => {
-    if (Notification.permission === 'granted') {
-    const options = {
-    body: 'Este lembrete expirou!',
-    icon: '/assets/icon/favicon.png',
-    sound: '/assets/audios/audioNotification.wav'
-    };
-    new Notification(title, options);
-    }
-    };
-    
-    React.useEffect(() => {
-    if ('Notification' in window) {
-    Notification.requestPermission();
-    }
-    }, []);
-    
-    React.useEffect(() => {
-    const cachedReminders = localStorage.getItem('reminders');
-    if (cachedReminders) {
-    setReminders(JSON.parse(cachedReminders));
-    }
-    }, []);
-    
-    React.useEffect(() => {
-    const intervalId = setInterval(() => {
-    handleCheckReminders();
-    }, 60000);
-    return () => clearInterval(intervalId);
-    }, [reminders]);
-    
-    return (
+const Home: React.FC = () => {
+  return (
     <IonPage>
       <IonHeader>
-        <IonToolbar>
-          <IonTitle>Lembretes</IonTitle>
+        <IonToolbar className='tututu'>
+          <IonAvatar className='img-avatar'><img src="/assets/icon/perfil.svg" alt="Imagem de perfil" /></IonAvatar>
         </IonToolbar>
       </IonHeader>
-    
-      <IonContent fullscreen>
-        <IonButton expand="block" onClick={() => setModalIsOpen(true)}>Adicionar novo lembrete</IonButton>
-      
-        <IonList>
-          {reminders.map(reminder => (
-            <IonItem key={reminder.id}>
-              <IonLabel>
-                <h2>{reminder.title}</h2>
-                <p>{reminder.date}</p>
-              </IonLabel>
-      
-              <IonButton color="danger" onClick={() => handleDeleteReminder(reminder.id)}>Excluir</IonButton>
-            </IonItem>
-          ))}
-        </IonList>
+         
+        
+          <div className='content-page'>
+            
+            <h2 className='principal-title'><strong>Olá Éric!</strong> Esses são seus quadros de lembretes.</h2>
 
-        <AddReminderModal isOpen={modalIsOpen} onSave={handleSaveReminder} onClose={() => setModalIsOpen(false)} />
-      </IonContent>
-    </IonPage>
-    );
-    };
-    
-    export default HomePage;
+          <div className="painel-reminders">
+            
+            <button className='btn-panel'>
+              <img src="/assets/icon/icon-cake.svg" alt="icon button" />
+              <h5>Aniversários</h5>
+            </button>
+
+            <button className='btn-panel'>
+              <img src="/assets/icon/icon-work.svg" alt="icon button" />
+              <h5>Trabalho</h5>
+            </button>
+
+            <button className='btn-panel'>
+              <img src="/assets/icon/icon-home.svg" alt="icon button" />
+              <h5>Casa</h5>
+            </button>
+
+            <button className='btn-panel'>
+              <img src="/assets/icon/icon-books.svg" alt="icon button" />
+              <h5>Faculdade</h5>
+            </button>
+
+            <button className='btn-panel-new'>
+              <img src="/assets/icon/icon-add.svg" alt="icon button" />
+              <h5>Criar</h5>
+            </button>
+          </div>
+
+          </div>
+
+
+      
+
+      
+    </IonPage> 
+  );
+};
+
+export default Home;
